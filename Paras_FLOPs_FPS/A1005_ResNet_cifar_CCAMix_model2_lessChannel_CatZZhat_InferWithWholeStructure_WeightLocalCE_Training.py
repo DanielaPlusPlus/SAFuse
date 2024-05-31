db9f8332311d9b29b1696155453b7be9a353c6d5
Daniela <63490892+DanielaPlusPlus@users.noreply.github.com>
2024-05-31 17:40:06 +0200

    Add files via upload

--- Paras_FLOPs_FPS/A1005_ResNet_cifar_CCAMix_model2_lessChannel_CatZZhat_InferWithWholeStructure_WeightLocalCE_Training.py
+++ Paras_FLOPs_FPS/A1005_ResNet_cifar_CCAMix_model2_lessChannel_CatZZhat_InferWithWholeStructure_WeightLocalCE_Training.py
@@ -155,14 +155,14 @@ class ResNet(nn.Module):
         # self.conv10 = conv(32, num_classes, kernel_size=7)
         # init.constant(self.conv10.weight, 0)  # Zero init
 
-        # self.SA = SelfAttention(input_size=64)
-        # self.SAP = self.SuperpixelAttentionPooling
+        self.SA = SelfAttention(input_size=64)
+        self.SAP = self.SuperpixelAttentionPooling
 
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         # self.fc = nn.Linear(512 * block.expansion, num_classes)
 
         self.fc2 = nn.Linear(2112, num_classes)
-        # self.fc_local = nn.Linear(16 * block.expansion, num_classes)
+        self.fc_local = nn.Linear(16 * block.expansion, num_classes)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -192,40 +192,40 @@ class ResNet(nn.Module):
         return nn.Sequential(*layers)
 
 
-    # def SuperpixelAttentionPooling(self, x, SuperP_mask, atten_top_ratio):
-    #     # print(x.shape)#torch.Size([32, 256, 32, 32])
-    #     avgpool_sa_batch_sel = []
-    #     avgpool_sa_batch = []
-    #     topN_idx_batch = []
-    #     for sp in range(x.shape[0]):
-    #         mask_value = np.unique(SuperP_mask[sp])
-    #         x_sp = x[sp].reshape(x.shape[2], x.shape[3], x.shape[1])
-    #
-    #         avgpool = []
-    #         for v in mask_value:
-    #             # print(x_sp.shape)
-    #             # print(x_sp[SuperP_mask[sp]==v].shape)
-    #             # print(x_sp[SuperP_mask[sp] == v].mean(0).shape)
-    #             avgpool.append(x_sp[SuperP_mask[sp]==v].mean(0))
-    #
-    #         avgpool = torch.stack(avgpool)
-    #
-    #         avgpool_sa = self.SA(avgpool)
-    #
-    #         avgpool_sa_spacial = avgpool_sa.sum(1)
-    #         _, map_topN_idx = torch.topk(avgpool_sa_spacial, int(atten_top_ratio*(avgpool_sa_spacial.shape[0])), dim=0, largest=True)
-    #
-    #         topN_idx_batch.append(map_topN_idx)
-    #         avgpool_sa_batch.append(avgpool_sa)
-    #
-    #         if len(map_topN_idx)>0:
-    #             avgpool_sa_sp = [avgpool_sa[idx] for idx in map_topN_idx]
-    #             avgpool_sa_sp = torch.stack(avgpool_sa_sp)
-    #             avgpool_sa_batch_sel.append(avgpool_sa_sp)
-    #         else:
-    #             avgpool_sa_batch_sel.append(avgpool_sa)
-    #
-    #     return avgpool_sa_batch, avgpool_sa_batch_sel, topN_idx_batch
+    def SuperpixelAttentionPooling(self, x, SuperP_mask, atten_top_ratio):
+        # print(x.shape)#torch.Size([32, 256, 32, 32])
+        avgpool_sa_batch_sel = []
+        avgpool_sa_batch = []
+        topN_idx_batch = []
+        for sp in range(x.shape[0]):
+            mask_value = np.unique(SuperP_mask[sp])
+            x_sp = x[sp].reshape(x.shape[2], x.shape[3], x.shape[1])
+
+            avgpool = []
+            for v in mask_value:
+                # print(x_sp.shape)
+                # print(x_sp[SuperP_mask[sp]==v].shape)
+                # print(x_sp[SuperP_mask[sp] == v].mean(0).shape)
+                avgpool.append(x_sp[SuperP_mask[sp]==v].mean(0))
+
+            avgpool = torch.stack(avgpool)
+
+            avgpool_sa = self.SA(avgpool)
+
+            avgpool_sa_spacial = avgpool_sa.sum(1)
+            _, map_topN_idx = torch.topk(avgpool_sa_spacial, int(atten_top_ratio*(avgpool_sa_spacial.shape[0])), dim=0, largest=True)
+
+            topN_idx_batch.append(map_topN_idx)
+            avgpool_sa_batch.append(avgpool_sa)
+
+            if len(map_topN_idx)>0:
+                avgpool_sa_sp = [avgpool_sa[idx] for idx in map_topN_idx]
+                avgpool_sa_sp = torch.stack(avgpool_sa_sp)
+                avgpool_sa_batch_sel.append(avgpool_sa_sp)
+            else:
+                avgpool_sa_batch_sel.append(avgpool_sa)
+
+        return avgpool_sa_batch, avgpool_sa_batch_sel, topN_idx_batch
 
     def forward(self, x, local=False, superpixel_map=None, topN_local_ratio=None):
         x0 = self.relu(self.bn1(self.conv1_3k3(x)))
@@ -260,25 +260,24 @@ class ResNet(nn.Module):
         pre_logit = pre_logit.reshape(pre_logit.size(0), -1)
         logits2 = self.fc2(pre_logit)
 
-        # if local:
-        #     x_sap_list, x_sap_sel_list, topN_local_idx = self.SAP(x_up5, superpixel_map, topN_local_ratio)  # list(32*[n, 256])
-        #     x_locals_out_batch = []
-        #     weights_locals_out_batch = []
-        #     for sp in range(len(x_sap_sel_list)):
-        #         x_locals_out_sp = []
-        #         for tk in range(x_sap_sel_list[sp].shape[0]):
-        #             x_local_out = self.fc_local(x_sap_sel_list[sp][tk])
-        #             x_locals_out_sp.append(x_local_out)
-        #         x_locals_out_sp = torch.stack(x_locals_out_sp)
-        #         x_locals_out_batch.append(x_locals_out_sp)
-        #         """normalizain in an image"""
-        #         weights_locals_out_batch.append(F.sigmoid(x_sap_list[sp].sum(1)))
-        #
-        #         # print(F.sigmoid(x_sap_list[sp].sum(1)))
-        #     return logits2, x_locals_out_batch, weights_locals_out_batch, topN_local_idx
-        # else:
-        #     return logits2
-        return logits2
+        if local:
+            x_sap_list, x_sap_sel_list, topN_local_idx = self.SAP(x_up5, superpixel_map, topN_local_ratio)  # list(32*[n, 256])
+            x_locals_out_batch = []
+            weights_locals_out_batch = []
+            for sp in range(len(x_sap_sel_list)):
+                x_locals_out_sp = []
+                for tk in range(x_sap_sel_list[sp].shape[0]):
+                    x_local_out = self.fc_local(x_sap_sel_list[sp][tk])
+                    x_locals_out_sp.append(x_local_out)
+                x_locals_out_sp = torch.stack(x_locals_out_sp)
+                x_locals_out_batch.append(x_locals_out_sp)
+                """normalizain in an image"""
+                weights_locals_out_batch.append(F.sigmoid(x_sap_list[sp].sum(1)))
+
+                # print(F.sigmoid(x_sap_list[sp].sum(1)))
+            return logits2, x_locals_out_batch, weights_locals_out_batch, topN_local_idx
+        else:
+            return logits2
 
 class Bottleneck(nn.Module):
     expansion = 4
@@ -349,62 +348,62 @@ class BasicBlock(nn.Module):
         return out
 
 
-# """https://blog.csdn.net/beilizhang/article/details/115282604"""
-#
-#
-# class LayerNorm(nn.Module):
-#     def __init__(self, hidden_size, eps=1e-12):
-#         """Construct a layernorm module in the TF style (epsilon inside the square root).
-#         """
-#         super(LayerNorm, self).__init__()
-#         self.weight = nn.Parameter(torch.ones(hidden_size))
-#         self.bias = nn.Parameter(torch.zeros(hidden_size))
-#         self.variance_epsilon = eps
-#
-#     def forward(self, x):
-#         u = x.mean(-1, keepdim=True)
-#         s = (x - u).pow(2).mean(-1, keepdim=True)
-#         x = (x - u) / torch.sqrt(s + self.variance_epsilon)
-#         return self.weight * x + self.bias
-#
-# class SelfAttention(nn.Module):
-#     def __init__(self, input_size):
-#         super(SelfAttention,self).__init__()
-#
-#         self.query = nn.Linear(input_size, input_size)
-#         self.key = nn.Linear(input_size, input_size)
-#         self.value = nn.Linear(input_size, input_size)
-#
-#         # self.out_dropout = nn.Dropout(dropout_prob)
-#         self.hidden_size = input_size
-#         self.LayerNorm = LayerNorm(input_size, eps=1e-12)
-#
-#
-#     def forward(self, input_tensor):
-#         """input tensor (n,d)"""
-#         query_layer = self.query(input_tensor)
-#         key_layer = self.key(input_tensor)
-#         value_layer = self.value(input_tensor)
-#
-#         # Take the dot product between "query" and "key" to get the raw attention scores.
-#         attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2))
-#         attention_scores = attention_scores / math.sqrt(self.hidden_size)
-#
-#         # Normalize the attention scores to probabilities.
-#         attention_probs = nn.Softmax(dim=-1)(attention_scores)
-#         # attention_probs = self.out_dropout(attention_probs)
-#
-#         hidden_states = torch.matmul(attention_probs, value_layer)
-#         hidden_states = self.LayerNorm(hidden_states + input_tensor)
-#
-#         return hidden_states
+"""https://blog.csdn.net/beilizhang/article/details/115282604"""
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, hidden_size, eps=1e-12):
+        """Construct a layernorm module in the TF style (epsilon inside the square root).
+        """
+        super(LayerNorm, self).__init__()
+        self.weight = nn.Parameter(torch.ones(hidden_size))
+        self.bias = nn.Parameter(torch.zeros(hidden_size))
+        self.variance_epsilon = eps
+
+    def forward(self, x):
+        u = x.mean(-1, keepdim=True)
+        s = (x - u).pow(2).mean(-1, keepdim=True)
+        x = (x - u) / torch.sqrt(s + self.variance_epsilon)
+        return self.weight * x + self.bias
+
+class SelfAttention(nn.Module):
+    def __init__(self, input_size):
+        super(SelfAttention,self).__init__()
+
+        self.query = nn.Linear(input_size, input_size)
+        self.key = nn.Linear(input_size, input_size)
+        self.value = nn.Linear(input_size, input_size)
+
+        # self.out_dropout = nn.Dropout(dropout_prob)
+        self.hidden_size = input_size
+        self.LayerNorm = LayerNorm(input_size, eps=1e-12)
+
+
+    def forward(self, input_tensor):
+        """input tensor (n,d)"""
+        query_layer = self.query(input_tensor)
+        key_layer = self.key(input_tensor)
+        value_layer = self.value(input_tensor)
+
+        # Take the dot product between "query" and "key" to get the raw attention scores.
+        attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2))
+        attention_scores = attention_scores / math.sqrt(self.hidden_size)
+
+        # Normalize the attention scores to probabilities.
+        attention_probs = nn.Softmax(dim=-1)(attention_scores)
+        # attention_probs = self.out_dropout(attention_probs)
+
+        hidden_states = torch.matmul(attention_probs, value_layer)
+        hidden_states = self.LayerNorm(hidden_states + input_tensor)
+
+        return hidden_states
 
 from fvcore.nn import FlopCountAnalysis
 net = resnet50(pretrained=False)
 net.fc = nn.Linear(2048, 100)
 net.fc_local = nn.Linear(64, 100)
 grid_mask = [torch.randint(10, size=(32,32))]
-tensor = (torch.randn(1, 3, 32, 32),) # 如果有多个参数就写成tuple形式
+tensor = (torch.randn(1, 3, 32, 32),True, grid_mask,0.7) # 如果有多个参数就写成tuple形式
 
 
 flops = FlopCountAnalysis(net, tensor)
